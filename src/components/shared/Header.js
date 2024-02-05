9dd79d1d3d87beee667040fb1bb452572e331910
Kawsar <web.kawsarahed@gmail.com>
2024-02-05 17:25:17 +0600

    added header section responsive design

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -7,8 +7,8 @@ import MobileMenu from "./MobileMenu";
 const Header = () => {
   return (
     <>
-      {/* <header className="border-b-[1px] border-divideColor">
-        <div className="flex items-center overflow-hidden">
+      <header className="font-ralewayMedium">
+        <div className="top flex border-b-[1px] border-divideColor">
           <div className="flex w-1/6 flex-col items-center">
             <a
               href="/"
@@ -27,10 +27,10 @@ const Header = () => {
             </a>
           </div>
 
-          <div className="flex h-[87px] w-5/6 items-center border-l-[1px] border-divideColor">
-            <nav className="w-3/5">
-              <ul className="font-ralewayMedium flex text-lg text-primary">
-                <li className="ml-3 text-center">
+          <div className="w-full flex h-[87px] items-center justify-between lg:border-l-[1px] lg:border-divideColor">
+            <nav>
+              <ul className="flex items-center text-lg text-primary">
+                <li className="text-center">
                   <Link
                     href="/songs"
                     className="px-3 hover:font-medium hover:text-fill"
@@ -56,162 +56,61 @@ const Header = () => {
                     Artist
                   </a>
                 </li>
-
-                <li className="text-center">
-                  <a
-                    href="/login"
-                    className="px-3 hover:font-medium hover:text-fill"
-                  >
-                    Login
-                  </a>
-                </li>
-
-                <li className="text-center">
-                  <a
-                    href="/register"
-                    className="px-3 hover:font-medium hover:text-fill"
-                  >
-                    Register
-                  </a>
-                </li>
               </ul>
             </nav>
 
-            <nav className="w-2/5">
-              <ul className="flex justify-end">
-                <li className="relative">
-                  <FaMagnifyingGlass className="absolute left-3 top-[9px] z-20 text-primary" />
+            {/* this section show only less than 768 mobile screen */}
+            <div className="dashboard mr-2 md:hidden">
+              <a href="/">
+                <Image
+                  src="/images/main_banner.jpg"
+                  className="w-8 rounded-full"
+                  alt="Profile Image"
+                  width={36}
+                  height={36}
+                />
+              </a>
+            </div>
 
-                  <input
-                    className="relative z-10 rounded-full bg-gray-200 px-8 py-1 font-ralewayRegular"
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Search Song"
-                  />
+            {/* this section show only large than 768 screen */}
+            <nav className="hidden md:block">
+              <ul className="flex items-center justify-end">
+                <li>
+                  <span className="relative">
+                    <span className="absolute left-2 top-[3px]">
+                      <FaMagnifyingGlass className="z-20 text-primary" />
+                    </span>
+
+                    <input
+                      className="z-10 rounded-full bg-gray-200 px-7 py-1 focus:outline-none"
+                      type="text"
+                      name="search"
+                      id="search"
+                      placeholder="Search Song"
+                    />
+                  </span>
                 </li>
 
-                <li className="ml-4">
-                  <a href="/dist/index.html">
-                    <img
+                <li className="mx-2">
+                  <a href="/">
+                    <Image
                       src="/images/main_banner.jpg"
                       className="w-7 rounded-full"
                       alt="Profile Image"
+                      width={28}
+                      height={28}
                     />
                   </a>
                 </li>
 
-                <li className="ml-2 mr-4">
-                  <a href="/dist/index.html">
-                    <FaBars className="text-2xl text-primary" />
+                <li className="mr-4">
+                  <a href="/">
+                    <FaBars className="text-[26px] text-primary" />
                   </a>
                 </li>
               </ul>
             </nav>
           </div>
-        </div>
-      </header> */}
-
-      <header className="font-ralewayMedium">
-        <div className="top flex justify-between items-center border-b-[1px] border-divideColor">
-          <nav>
-            <ul className="flex items-center text-lg text-primary">
-              <li className="text-center">
-                <a
-                  href="/"
-                  className="flex flex-col items-center hover:font-medium hover:text-fill"
-                >
-                  <Image
-                    className="-mb-2 w-20"
-                    src="/images/logo.png"
-                    alt="Logo Image"
-                    width={200}
-                    height={100}
-                  />
-                  <span className="text-[10px]">Blue Pie Meta</span>
-                </a>
-              </li>
-
-              <li className="text-center">
-                <Link
-                  href="/songs"
-                  className="px-3 hover:font-medium hover:text-fill"
-                >
-                  Songs
-                </Link>
-              </li>
-
-              <li className="text-center">
-                <a
-                  href="/projects"
-                  className="px-3 hover:font-medium hover:text-fill"
-                >
-                  Projects
-                </a>
-              </li>
-
-              <li className="text-center">
-                <a
-                  href="/artist"
-                  className="px-3 hover:font-medium hover:text-fill"
-                >
-                  Artist
-                </a>
-              </li>
-            </ul>
-          </nav>
-
-          {/* this section show only less than 768 mobile screen */}
-          <div className="dashboard mr-2 md:hidden">
-            <a href="/">
-              <Image
-                src="/images/main_banner.jpg"
-                className="w-8 rounded-full"
-                alt="Profile Image"
-                width={36}
-                height={36}
-              />
-            </a>
-          </div>
-
-          {/* this section show only large than 768 screen */}
-          <nav className="hidden md:block">
-            <ul className="flex items-center justify-end">
-              <li>
-                <span className="relative">
-                  <span className="absolute left-2 top-[3px]">
-                    <FaMagnifyingGlass className="z-20 text-primary" />
-                  </span>
-
-                  <input
-                    className="z-10 rounded-full bg-gray-200 px-7 py-1 focus:outline-none"
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Search Song"
-                  />
-                </span>
-              </li>
-
-              <li className="mx-2">
-                <a href="/">
-                  <Image
-                    src="/images/main_banner.jpg"
-                    className="w-7 rounded-full"
-                    alt="Profile Image"
-                    width={28}
-                    height={28}
-                  />
-                </a>
-              </li>
-
-              <li className="mr-4">
-                <a href="/">
-                  <FaBars className="text-[26px] text-primary" />
-                </a>
-              </li>
-            </ul>
-          </nav>
         </div>
 
         {/* this section show only less than 768 mobile screen */}
@@ -240,7 +139,7 @@ const Header = () => {
         </div>
 
         {/* mobile menu show only mobile screen */}
-        <div className="hidden mobile-menu  bg-slate-400 absolute left-0 top-0 w-full h-full">
+        <div className=" mobile-menu  bg-slate-400 absolute left-0 top-0 w-full h-full">
           <MobileMenu />
         </div>
       </header>
